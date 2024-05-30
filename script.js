fe41fc275b43f77223cfb63218bb4f76305a33e9
Nipe Setiawan Harefa <nipeharefa@gmail.com>
2024-05-30 18:04:05 +0700

    add hours

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ const dynamicIsland = document.querySelector('.dynamic-island');
 dynamicIsland.addEventListener('click', (e) => {
     e.target.classList.toggle('active');
 })
+
+
+const jam = document.querySelector('h1.jam')
+
+const tampilkanJam = () => {
+    const now = new Date();
+    // console.log(now)
+
+    const options = {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: false
+    }
+
+    const text = Intl.DateTimeFormat(undefined, options).format(now)
+    // console.log(text)
+    jam.textContent = text;
+};
+
+
+setInterval(tampilkanJam, 1000)
